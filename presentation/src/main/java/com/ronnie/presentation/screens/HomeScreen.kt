44d6fbc7a9d45cc10bd0abf30c8f089f2296a8af
Ronnie Otieno <ronnieotieno2@gmail.com>
2022-09-04 15:50:11 +0300

    Add Header and NYT logo
    Signed-off-by: Ronnie Otieno <ronnieotieno2@gmail.com>

--- presentation/src/main/java/com/ronnie/presentation/screens/HomeScreen.kt
+++ presentation/src/main/java/com/ronnie/presentation/screens/HomeScreen.kt
@@ -2,11 +2,16 @@ package com.ronnie.presentation.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
@@ -14,13 +19,25 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.ronnie.domain.models.UiState
 import com.ronnie.presentation.NewsViewModel
+import com.ronnie.presentation.R
 import com.ronnie.presentation.ShowData
 import com.ronnie.presentation.components.ChipGroup
 import com.ronnie.presentation.components.LoadingShimmerEffect
@@ -46,8 +63,27 @@ fun HomeScreen(viewModel: NewsViewModel = hiltViewModel()){
             .fillMaxSize()
             .padding(10.dp)
     ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(R.drawable.nyt_logo)
+                .crossfade(true)
+                .build(),
+            contentDescription = "news Image",
+            modifier = Modifier
+                .fillMaxWidth().padding(end= 10.dp, start = 10.dp),
+            contentScale = ContentScale.Inside
+        )
+        Text(
+            text = "Top Stories",
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.primaryTextColor),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = TextStyle(fontSize = 30.sp, fontFamily = FontFamily.Serif),
+            maxLines = 1,
+        )
+        Spacer(Modifier.height(5.dp))
         ChipGroup(newsCategories, selectedCategory = selectedCategory.value,onSelectedChanged = {
-            Log.d("SelectedCategory", it)
             selectedCategory.value = it
             selectedCategory.value?.lowercase(Locale.ROOT).let { category-> viewModel.getNews(category!!) }
         })
