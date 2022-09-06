e0b617e54365be66f1b47d1c1f838415490f0261
Ronnie Otieno <ronnieotieno2@gmail.com>
2022-09-06 15:30:27 +0300

    only use light theme
    Signed-off-by: Ronnie Otieno <ronnieotieno2@gmail.com>

--- presentation/src/main/java/com/ronnie/presentation/theme/Theme.kt
+++ presentation/src/main/java/com/ronnie/presentation/theme/Theme.kt
@@ -29,11 +29,7 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun NYTNewsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
